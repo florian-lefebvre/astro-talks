591a71ec7b20e4ab2aa4120b47ff01ed6340c674
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-04-06 15:27:55 +0200

    prepare demo

--- demos/2026-04-astrotogether/astro.config.mjs
+++ demos/2026-04-astrotogether/astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
+// TODO:
 import { defineConfig } from 'astro/config';
+// import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // fonts: [
+    //     {
+    //         name: 'Public Sans',
+    //         cssVariable: '--font-body',
+    //         provider: fontProviders.fontsource(),
+    //         weights: ['400', '700']
+    //     },
+    //     {
+    //         name: 'Rubik',
+    //         cssVariable: '--font-brand',
+    //         provider: fontProviders.fontsource(),
+    //         weights: ['500', '600'],
+    //         styles: ['normal']
+    //     }
+    // ]
+});
